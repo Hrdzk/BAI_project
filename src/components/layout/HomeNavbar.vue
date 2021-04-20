4be75106c8bc77d41cd517c303bd6c81df17713e
Maksymilian Bobrowski <mboberek97@gmail.com>
2021-04-20 11:32:06 +0200

    Delete src directory

--- src/components/layout/HomeNavbar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="container">
-    <router-link to="/readbooks"><BaseButton>Books</BaseButton></router-link>
-    <router-link to="/randombook"
-      ><BaseButton>Random book</BaseButton></router-link
-    >
-  </div>
-</template>
